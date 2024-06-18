e9ff63b0cf7bd9b3501adabc41f14036f93fee25
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-06-18 18:11:45 -0300

    métodos de classes, propety...

---
+++ Python/curso - poo 1/models/restaurant.py
@@ -0,0 +1,24 @@
+class Restaurante:
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self._nome} || {self._categoria}'
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Ativo'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '❌'
+
+    def alterar_estado(self):
+        self._ativo = not self._ativo
