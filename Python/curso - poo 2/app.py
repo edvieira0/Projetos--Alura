91cb644093f23fb02ca80f8a9756df48e4fe7a1f
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-06-18 22:54:48 -0300

    aplicando polimorfismo

--- Python/curso - poo 2/app.py
+++ Python/curso - poo 2/app.py
@@ -7,9 +7,11 @@ restaurante_praca = Restaurante('praça', 'brasileira')
 restaurante_beto = Restaurante('beto food', 'mexicana')
 restaurante_irmaos = Restaurante('two brothers', 'japonesa')
 
-bebida_1 = Bebida('Coca', 5.40, 'pequena')
+bebida_1 = Bebida('Coca', 5.50, 'pequena')
+bebida_1.aplicar_desconto()
 
 prato_1 = Prato('Feijoada', 7.50, 'Feijão preto com linguiça')
+prato_1.aplicar_desconto()
 
 restaurante_praca.adicionar_cardapio(bebida_1)
 restaurante_praca.adicionar_cardapio(prato_1)
--- Python/curso - poo 2/models/cardapio/bebida.py
+++ Python/curso - poo 2/models/cardapio/bebida.py
@@ -4,6 +4,9 @@ class Bebida(ItemCardapio):
     def __init__(self, nome, preco, tamanho):
         super().__init__(nome, preco)
         self.tamanho = tamanho
-    
+
     def __str__(self):
         return self._nome
+    
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
