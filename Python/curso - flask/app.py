15f44c4814b1846ddcb1189cf76c442c6178088b
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-06-21 13:34:34 -0300

    reproduzindo listas, estrutura do for no html

--- Python/curso - flask/app.py
+++ Python/curso - flask/app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jogos = ['God of war', 'Red dead Redemption', 'Skyrim', 'Need for Speed', 'The last of us', 'Battlefield 4']
+
 @app.route('/inicio')
 def incio():
-    return render_template('lista.html', titulo='Jogos')
+    return render_template('lista.html', titulo='Jogos', lista_jogos=jogos)
 
 app.run()
+
+
+# Loop é feito no arquivo onde vai ser reproduzido a lista com a sintaxe de -> {% for i in lista %} <tr><td>{{ i }}</td></tr> e fechando em {% endfor %}
