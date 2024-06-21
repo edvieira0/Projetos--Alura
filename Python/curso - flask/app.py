854bd2fa03b3fe6c582cb6482dac7d2449ae0a57
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-06-21 13:50:39 -0300

    aplicando poo e adicionando novos jogos

--- Python/curso - flask/app.py
+++ Python/curso - flask/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
-jogos = ['God of war', 'Red dead Redemption', 'Skyrim', 'Need for Speed', 'The last of us', 'Battlefield 4']
+app = Flask(__name__)
 
 @app.route('/inicio')
 def incio():
-    return render_template('lista.html', titulo='Jogos', lista_jogos=jogos)
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God Of War', 'Rack n slash', 'Ps2')
+    jogo3 = Jogo('Battlefield 4', 'First Person Shooter', 'Pc')
+    jogo4 = Jogo('Red Dead Redemption', 'Open World', 'Pc')
+
+    lista_jogos = [jogo1, jogo2, jogo3, jogo4]
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 app.run()
 
