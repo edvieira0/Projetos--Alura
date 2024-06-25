4ff3f13394177447ef2ad05d70dcd7808abe7261
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-06-25 19:44:05 -0300

    jogo final

--- C e C++/03. Jogo Pac Man/mapa.c
+++ C e C++/03. Jogo Pac Man/mapa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mapa.h"
+#include <string.h>
 
 void lemapa(MAPA* m) {
 	FILE* f;
@@ -28,6 +29,16 @@ void alocamapa(MAPA* m) {
 	}
 }
 
+void copiamapa(MAPA* destino, MAPA* origem) {
+	destino->linhas = origem->linhas;
+	destino->colunas = origem->colunas;
+	alocamapa(destino);
+	for(int i = 0; i < origem->linhas; i++) {
+		strcpy(destino->matriz[i], origem->matriz[i]);
+	}
+}
+
+
 void liberamapa(MAPA* m) {
 	for(int i = 0; i < m->linhas; i++) {
 		free(m->matriz[i]);
@@ -36,22 +47,55 @@ void liberamapa(MAPA* m) {
 	free(m->matriz);
 }
 
-void imprimemapa(MAPA* m) {
-	for(int i = 0; i < m->linhas; i++) {
-		printf("%s\n", m->matriz[i]);
-	}
-}
 
-void encontramapa(MAPA* m, POSICAO* p, char c) {
+int encontramapa(MAPA* m, POSICAO* p, char c) {
 
 	for(int i = 0; i < m->linhas; i++) {
 		for(int j = 0; j < m->colunas; j++) {
 			if(m->matriz[i][j] == c) {
 				p->x = i;
 				p->y = j;
-				return;
+				return 1;
 			}
 		}
 	}
+
+	return 0;
+}
+
+int podeandar(MAPA* m, char personagem, int x, int y) {
+	return 
+		ehvalida(m, x, y) && 
+		!ehparede(m, x, y) &&
+		!ehpersonagem(m, personagem, x, y);
+}
+
+int ehvalida(MAPA* m, int x, int y) {
+	if(x >= m->linhas) 
+		return 0;
+	if(y >= m->colunas) 
+		return 0;
+
+	return 1;	
+}
+
+int ehpersonagem(MAPA* m, char personagem, int x, int y) {
+	return
+		m->matriz[x][y] == personagem;
+}
+
+int ehparede(MAPA* m, int x, int y) {
+	return 
+		m->matriz[x][y] == PAREDE_VERTICAL ||
+		m->matriz[x][y] == PAREDE_HORIZONTAL;
+}
+
+
+void andanomapa(MAPA* m, int xorigem, int yorigem, 
+	int xdestino, int ydestino) {
+
+	char personagem = m->matriz[xorigem][yorigem];
+	m->matriz[xdestino][ydestino] = personagem;
+	m->matriz[xorigem][yorigem] = VAZIO;
 
 }
--- C e C++/03. Jogo Pac Man/mapa.h
+++ C e C++/03. Jogo Pac Man/mapa.h
@@ -1,3 +1,14 @@
+#ifndef _MAPA_H_
+#define _MAPA_H_
+
+
+#define HEROI '@'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define FANTASMA 'F'
+#define PILULA 'P'
+
 struct mapa {
 	char** matriz;
 	int linhas;
@@ -9,7 +20,6 @@ typedef struct mapa MAPA;
 void alocamapa(MAPA* m);
 void lemapa(MAPA* m);
 void liberamapa(MAPA* m);
-void imprimemapa(MAPA* m);
 
 struct posicao {
 	int x;
@@ -18,4 +28,17 @@ struct posicao {
 
 typedef struct posicao POSICAO;
 
-void encontramapa(MAPA* m, POSICAO* p, char c);
+int encontramapa(MAPA* m, POSICAO* p, char c);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem, 
+	int xdestino, int ydestino);
+
+void copiamapa(MAPA* destino, MAPA* origem);
+
+int podeandar(MAPA* m, char personagem, int x, int y);
+
+#endif
