b2050939438d1c0310e797fc54a1325540861632
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-07-30 20:12:28 -0300

    funcao de pontos do basquete

--- Python/desafio - discord/app.py
+++ Python/desafio - discord/app.py
@@ -4,5 +4,13 @@
 
 
 # Segundo desafio, retornar a idade de anos para dias.
-def calAge(idade):
-    return idade * 365
+# def calAge(idade):
+#     return idade * 365
+
+
+def points(dois, tres):
+    dois *= 2
+    tres *= 3
+    return dois + tres
+
+print(points(7, 5))
