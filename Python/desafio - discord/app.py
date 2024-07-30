410d714541621f6e97b1d4d5ef42f83d71c28544
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-07-30 20:01:47 -0300

    funcao de conversao de anos para dias

--- Python/desafio - discord/app.py
+++ Python/desafio - discord/app.py
@@ -1,3 +1,8 @@
 # Primeiro desafio, retonar a soma de dois numeros.
 # def sum(a, b):
 #     return a + b
+
+
+# Segundo desafio, retornar a idade de anos para dias.
+def calAge(idade):
+    return idade * 365
