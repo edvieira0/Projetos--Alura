2cd78367edd459ad9e0deb9f5bd448676dfa9ac3
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-07-30 20:53:49 -0300

    sequencia de fibonacci

--- Python/desafio - discord/app.py
+++ Python/desafio - discord/app.py
@@ -14,12 +14,22 @@
 #     return dois + tres
 
 
-def countTrue(lista):
-    j = 0
-    for i in lista:
-        if i:
-            j += 1
-    return j
+# def countTrue(lista):
+#     j = 0
+#     for i in lista:
+#         if i:
+#             j += 1
+#     return j
 
 # def countTrue(lst):
 #   return len([value for value in lst if value])
+
+
+def fibonaccin(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        print(a)
+        a, b = b, a + b
+
+print(fibonaccin(10))
